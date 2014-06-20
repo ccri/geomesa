b4f89b9c3bc7e805fea4c5f152f35257f1daf39a
Andrew Annex <aannex@ccri.com>
2014-06-20 10:07:16 -0400

    GEOMESA-131 AvroSimpleFeature now implements getProperties functions, tests written in AvroSimpleFeatureTest

--- geomesa-core/src/test/scala/geomesa/core/avro/AvroSimpleFeatureTest.scala
+++ geomesa-core/src/test/scala/geomesa/core/avro/AvroSimpleFeatureTest.scala
@@ -8,6 +8,8 @@ import org.geotools.filter.identity.FeatureIdImpl
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
+import java.util
+import org.opengis.feature.Property
 
 @RunWith(classOf[JUnitRunner])
 class AvroSimpleFeatureTest extends Specification {
@@ -24,6 +26,19 @@ class AvroSimpleFeatureTest extends Specification {
       f.getAttribute(0) must beAnInstanceOf[java.lang.Integer]
       f.getAttribute(1) must beAnInstanceOf[java.util.Date]
       f.getAttribute(2) must beAnInstanceOf[Geometry]
+    }
+
+    "properly return all requested properties" in {
+      val sft = DataUtilities.createType("testType", "a:Integer,b:Date,*geom:Point:srid=4326")
+
+      val f = new AvroSimpleFeature(new FeatureIdImpl("fakeid"), sft)
+      f.setAttribute(0,"1")
+      f.setAttribute(1,"2013-01-02T00:00:00.000Z")
+      f.setAttribute(2,"POINT(45.0 49.0)")
+
+      f.getProperties must beAnInstanceOf[util.Collection[Property]]
+      f.getProperties("a") must beAnInstanceOf[util.Collection[Property]]
+      f.getProperties("a").head.getValue must not(throwA [org.opengis.feature.IllegalAttributeException])
     }
   }
 
