f65f520166f9e523fd37f46d3c5526d85130cba4
Jim Hughes <jnh5y@ccri.com>
2014-08-11 08:24:33 -0400

    Ok, tests pass now.

--- geomesa-core/src/main/scala/geomesa/core/index/IndexQueryPlanner.scala
+++ geomesa-core/src/main/scala/geomesa/core/index/IndexQueryPlanner.scala
@@ -421,7 +421,7 @@ case class IndexQueryPlanner(keyPlanner: KeyPlanner,
 
       cfg.addOption(DEFAULT_SCHEMA_NAME, schema)
       configureFeatureEncoding(cfg)
-      configureTransforms(query,cfg)
+      //configureTransforms(query,cfg)
       configureFeatureType(cfg, featureType)
       ecql.foreach(SimpleFeatureFilteringIterator.setECQLFilter(cfg, _))
 
