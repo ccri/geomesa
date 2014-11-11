88074fb3b820e340b1cf6c20787aefeffe4b5d39
Jianping Wang <jw9bn@ccri.com>
2014-11-11 15:59:53 -0500

    GEOMESA-433,GEOMESA-434,GEOMESA-434 Develop raster and ingest handling code for integration with GeoMesa. Implement raster ingest as a new maven submodule. Create a new Store for raster data.

--- geomesa-core/src/main/scala/org/locationtech/geomesa/core/data/AccumuloDataStoreFactory.scala
+++ geomesa-core/src/main/scala/org/locationtech/geomesa/core/data/AccumuloDataStoreFactory.scala
@@ -27,7 +27,7 @@ import org.apache.accumulo.core.client.{Connector, ZooKeeperInstance}
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.mapreduce.Job
 import org.geotools.data.DataAccessFactory.Param
-import org.geotools.data.DataStoreFactorySpi
+import org.geotools.data.{AbstractDataStoreFactory, DataStoreFactorySpi}
 import org.locationtech.geomesa.core.security.{AuthorizationsProvider, DefaultAuthorizationsProvider, FilteringAuthorizationsProvider}
 import org.locationtech.geomesa.core.stats.StatWriter
 
@@ -202,6 +202,8 @@ object AccumuloDataStoreFactory {
     val tableNameParam      = new Param("tableName", classOf[String], "The Accumulo Table Name", true)
     val queryThreadsParam   = new Param("queryThreads", classOf[Integer], "The number of threads to use per query", false)
     val recordThreadsParam  = new Param("recordThreads", classOf[Integer], "The number of threads to use for record retrieval", false)
+    val shardsParam         = new Param("shards", classOf[Integer], "Accumulo number of shards to use for writing coverage data", false)
+    val writeMemoryParam    = new Param("writeMemory", classOf[Integer], "The memory allocation to use for writing records", false)
     val writeThreadsParam   = new Param("writeThreads", classOf[Integer], "The number of threads to use for writing records", false)
     val statsParam          = new Param("collectStats", classOf[java.lang.Boolean], "Toggle collection of statistics", false, java.lang.Boolean.TRUE)
     val cachingParam        = new Param("caching", classOf[java.lang.Boolean], "Toggle caching of results", false, java.lang.Boolean.TRUE)
