3c23322c60aa86a1a1a505dc9df9646f9b07953b
Jim Hughes <jnh5y@ccri.com>
2014-12-16 15:20:25 -0500

    First round of comments.

--- geomesa-core/src/main/scala/org/locationtech/geomesa/core/data/AccumuloDataStore.scala
+++ geomesa-core/src/main/scala/org/locationtech/geomesa/core/data/AccumuloDataStore.scala
@@ -715,15 +715,25 @@ class AccumuloDataStore(val connector: Connector,
   def writeTemporalBounds(featureName: String, timeBounds: Interval) {
     val newTimeBounds = metadata.read(featureName, TEMPORAL_BOUNDS_KEY) match {
       case Some(currentTimeBoundsString) => getNewTimeBounds(currentTimeBoundsString, timeBounds)
-      case None                          => timeBounds
+      case None                          => Some(timeBounds)
     }
 
-    val encoded = s"${newTimeBounds.getStartMillis}:${newTimeBounds.getEndMillis}"
-
-    metadata.insert(featureName, TEMPORAL_BOUNDS_KEY, encoded)
+    // Only write expanded bounds.
+    newTimeBounds.foreach { newBounds =>
+      val encoded = s"${newBounds.getStartMillis}:${newBounds.getEndMillis}"
+      metadata.insert(featureName, TEMPORAL_BOUNDS_KEY, encoded)
+    }
   }
 
-  def getNewTimeBounds(current: String, newBounds: Interval) = stringToTimeBounds(current).expandByInterval(newBounds)
+  def getNewTimeBounds(current: String, newBounds: Interval): Option[Interval] = {
+    val currentTimeBounds = stringToTimeBounds(current)
+    val expandedTimeBounds = currentTimeBounds.expandByInterval(newBounds)
+    if (!currentTimeBounds.equals(expandedTimeBounds)) {
+      Some(expandedTimeBounds)
+    } else {
+      None
+    }
+  }
 
   /**
    * Implementation of abstract method
