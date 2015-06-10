b324b0c0a7241728ae105a44c3d95bf94762511c
Michael Matz <mmatz@ccri.com>
2015-06-10 14:10:51 -0400

    misc cleanup
    Signed-off-by: Michael Matz <mmatz@ccri.com>

--- geomesa-kafka/geomesa-kafka-geoserver-plugin/src/main/java/org/locationtech/geomesa/kafka/plugin/GeomesaKafkaProcessFactory.java
+++ geomesa-kafka/geomesa-kafka-geoserver-plugin/src/main/java/org/locationtech/geomesa/kafka/plugin/GeomesaKafkaProcessFactory.java
@@ -5,10 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GeomesaKafkaProcessFactory extends SpringBeanProcessFactory {
-    private Logger log = LoggerFactory.getLogger(GeomesaKafkaProcessFactory.class);
+    private static final Logger logger = LoggerFactory.getLogger(GeomesaKafkaProcessFactory.class);
 
     public GeomesaKafkaProcessFactory(String title, String namespace, Class markerInterface) {
         super(title, namespace, markerInterface);
-        log.info("Created GeomesaKafkaProcessFactory");
+        logger.info("Created GeomesaKafkaProcessFactory");
     }
 }
