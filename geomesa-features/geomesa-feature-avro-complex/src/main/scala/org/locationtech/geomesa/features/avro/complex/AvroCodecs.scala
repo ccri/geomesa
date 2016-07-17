371ace1f37a8ddcf1875cf0954c7c0d7c231233b
Jim Hughes <jnh5y@ccri.com>
2016-07-17 18:00:46 -0400

    Example of adding a new copyright header for non-CCRI contributions.

--- geomesa-features/geomesa-feature-avro-complex/src/main/scala/org/locationtech/geomesa/features/avro/complex/AvroCodecs.scala
+++ geomesa-features/geomesa-feature-avro-complex/src/main/scala/org/locationtech/geomesa/features/avro/complex/AvroCodecs.scala
@@ -1,3 +1,11 @@
+/***********************************************************************
+* Copyright (c) 2016 FSI
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0
+* which accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*************************************************************************/
+
 package org.locationtech.geomesa.features.avro.complex
 
 import java.nio.ByteBuffer
