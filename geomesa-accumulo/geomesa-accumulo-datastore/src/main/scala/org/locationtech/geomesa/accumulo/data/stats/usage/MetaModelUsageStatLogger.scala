63fc028c5caea4350dc9cac6103456d431140c5f
Charles Kelly <charles.kelly@ccri.com>
2016-07-27 15:13:48 -0400

    JsonStatWriter writes to a log file that Filebeats can send off to Logstash
    Signed-off-by: Charles Kelly <charles.kelly@ccri.com>

---
+++ geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/data/stats/usage/MetaModelUsageStatLogger.scala
@@ -0,0 +1,10 @@
+package org.locationtech.geomesa.accumulo.data.stats.usage
+
+/**
+  * Created by ckelly on 7/6/16.
+  */
+class MetaModelUsageStatLogger{
+  def log(writer : UsageStatWriter) : Unit = {
+
+  }
+}
--- geomesa-accumulo/geomesa-accumulo-json-stats/src/main/scala/org/locationtech/geomesa/accumulo/data/stats/usage/JsonStatWriter.scala
+++ geomesa-accumulo/geomesa-accumulo-json-stats/src/main/scala/org/locationtech/geomesa/accumulo/data/stats/usage/JsonStatWriter.scala
@@ -1,15 +1,22 @@
 package org.locationtech.geomesa.accumulo.data.stats.usage
 
-import org.locationtech.geomesa.accumulo.data.stats.usage
+import java.io.{File, FileOutputStream, PrintWriter}
 
+import com.google.gson.GsonBuilder
 
 class JsonStatWriter extends UsageStatWriter {
   //def queueStat[T <: UsageStat](stat: T)(implicit transform: UsageStatTransform[T]): Unit
 
   //def queueStat[T <: UsageStat](stat: T)(implicit transform: UsageStatTransform[T]): Unit
   override def writeStat[T <: UsageStat](stat: T): Unit = {
-    println(s"Supposed to write a stat to a file! $stat")
 
+    val gson = new GsonBuilder()
+      .serializeNulls()
+      .create()
+    println(s"Supposed to write a stat to a file! ${gson.toJson(stat)}")
+    val out = new PrintWriter(new FileOutputStream(new File("/tmp/json-logs/qs.log"),true))
+    out.write(gson.toJson(stat)+"\n")
+    out.close()
   }
 
   override def close(): Unit = {
