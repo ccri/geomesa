f677a86f3f4cdc1e42a2f8727246c978103bb2fe
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2016-11-15 11:18:06 -0500

    wip more caches

--- geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/iterators/IteratorCache.scala
+++ geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/iterators/IteratorCache.scala
@@ -10,18 +10,27 @@ package org.locationtech.geomesa.accumulo.iterators
 
 import org.locationtech.geomesa.features.SerializationOption.{SerializationOption, SerializationOptions}
 import org.locationtech.geomesa.features.kryo.{KryoBufferSimpleFeature, KryoFeatureSerializer}
+import org.locationtech.geomesa.filter.factory.FastFilterFactory
 import org.locationtech.geomesa.utils.cache.SoftThreadLocalCache
 import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
 import org.opengis.feature.simple.SimpleFeatureType
+import org.opengis.filter.Filter
 
 object IteratorCache {
 
   private val sftCache = new SoftThreadLocalCache[(String, String), SimpleFeatureType]()
+  private val serializerCache = new SoftThreadLocalCache[(SimpleFeatureType, SerializationOptions), KryoFeatureSerializer]()
   private val kryoBufferCache = new SoftThreadLocalCache[(SimpleFeatureType, SerializationOptions), KryoBufferSimpleFeature]()
+  private val filterCache = new SoftThreadLocalCache[String, Filter]()
 
   def sft(name: String, spec: String): SimpleFeatureType =
     sftCache.getOrElseUpdate((name, spec), SimpleFeatureTypes.createType(name, spec))
 
+  def serializer(sft: SimpleFeatureType, options: Set[SerializationOption]): KryoFeatureSerializer =
+    serializerCache.getOrElseUpdate((sft, options), new KryoFeatureSerializer(sft, options))
+
   def kryoBufferFeature(sft: SimpleFeatureType, options: Set[SerializationOption]) =
-    kryoBufferCache.getOrElseUpdate((sft, options), new KryoFeatureSerializer(sft, options).getReusableFeature)
+    kryoBufferCache.getOrElseUpdate((sft, options), serializer(sft, options).getReusableFeature)
+
+  def filter(ecql: String): Filter = filterCache.getOrElseUpdate(ecql, FastFilterFactory.toFilter(ecql))
 }
