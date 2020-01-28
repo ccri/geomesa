e0d81fad69b3527708d3a264c32602d8d34dca4b
Jim Hughes <jnh5y@ccri.com>
2020-01-28 16:28:48 -0500

    LinkedBlockingQueues for the win.
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

--- geomesa-hbase/geomesa-hbase-datastore/src/main/scala/org/locationtech/geomesa/hbase/coprocessor/GeoMesaCoprocessor.scala
+++ geomesa-hbase/geomesa-hbase-datastore/src/main/scala/org/locationtech/geomesa/hbase/coprocessor/GeoMesaCoprocessor.scala
@@ -240,7 +240,7 @@ object GeoMesaCoprocessor extends LazyLogging {
       threads: Int
     ) extends CloseableIterator[ByteString] {
 
-    private val pool = new ThreadPoolExecutor(1, threads, 60, TimeUnit.SECONDS, new SynchronousQueue[Runnable])
+    private val pool = new ThreadPoolExecutor(1, threads, 60, TimeUnit.SECONDS, new LinkedBlockingQueue[Runnable])
     private val htable = connection.getTable(table, pool)
     private val closed = new AtomicBoolean(false)
 
